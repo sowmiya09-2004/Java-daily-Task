cc71cf62a536a2c2c6ed75dc4ddde36acbcfd24e
sowmiya09-2004 <146425679+sowmiya09-2004@users.noreply.github.com>
2024-09-23 16:26:53 +0530

    Add files via upload

---
+++ Scanner/src/pratice/method.java
@@ -0,0 +1,35 @@
+package pratice;
+
+import java.util.Scanner;
+
+public class method {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String next = sc.next();
+		System.out.println(next);
+		
+		String d=sc.nextLine();
+		System.out.println(d);
+		
+		byte nextByte = sc.nextByte();
+		System.out.println(nextByte);
+		
+		int nextInt = sc.nextInt();
+		System.out.println(nextInt);
+		
+		boolean nextBoolean = sc.nextBoolean();
+		System.out.println(nextBoolean);
+		
+		double nextDouble = sc.nextDouble();
+		System.out.println(nextDouble);
+		
+		float nextFloat = sc.nextFloat();
+		System.out.println(nextFloat);
+		
+		long nextLong = sc.nextLong();
+		System.out.println(nextLong);
+		
+	}
+
+}
