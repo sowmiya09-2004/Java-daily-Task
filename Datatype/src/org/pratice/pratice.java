5d769179d12c9fd9d22f20d42912932aff184f83
sowmiya09-2004 <146425679+sowmiya09-2004@users.noreply.github.com>
2024-09-23 16:19:00 +0530

    Add files via upload

---
+++ Datatype/src/org/pratice/pratice.java
@@ -0,0 +1,29 @@
+package org.pratice;
+
+public class pratice {
+	public static void main(String[] args) {
+		long a=123456789909864l;
+		System.out.println("the long datatype is"+a);
+	    byte age=21;
+	    System.out.println("the byte datatype is"+age);
+	    short salary=30000;
+	    System.out.println("the short datatype is "+salary);
+	    int  b=123456789;
+	    System.out.println("the int datatype is "+b);
+	    float f=2.456f;
+	    System.out.println("the float data type is"+f);
+	    double d= 34.456788908766;
+	    System.out.println("the double datatype is"+d);
+	    boolean t=true;
+	    System.out.println("the boolean datatype is"+t);
+	    char g='c';
+	    System.out.println("the character is"+g);
+	    String name="chintu";
+	    System.out.println("the strin datatype is"+name);
+	    
+	    
+	    
+	    
+	}
+
+}
---
+++ Datatype/src/org/pratice/simple.java
@@ -0,0 +1,21 @@
+package org.pratice;
+
+public class simple {
+	
+	public static void main(String[] args) {
+	long a=123456789909864l;
+    byte age=21;
+    short salary=30000;
+    int  b=123456789;
+    float f=2.456f;
+    double d= 34.456788908766;
+    boolean t=true;
+    char g='c';
+    String name="chintu";
+    System.out.println("the long datatype is:"+a+"\n"+"the byte datatype is:"+age+"\n"+"the short datatype is :"+salary+"\n"+"the int datatype is: "+b
+    		+"\n"+"the float data type is:"+f+"\n"+"the double datatype is:"+d+"\n"+"the boolean datatype is:"+t+"\n"+"the character is:"+g
+    		+"\n"+"the strin datatype is:"+name);
+    
+
+}
+}
