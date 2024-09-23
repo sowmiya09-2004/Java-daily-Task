8058b9ba025a19b82b4140b3dba776501c2c6204
sowmiya09-2004 <146425679+sowmiya09-2004@users.noreply.github.com>
2024-09-23 16:23:18 +0530

    Add files via upload

---
+++ green address/src/org/add/GreensTech.java
@@ -0,0 +1,36 @@
+package org.add;
+
+public class GreensTech {
+	private void greensOmr() {
+		System.out.println("Greens Omr");
+		
+	}
+	private void greensAdayar() {
+		System.out.println("Greens Adayar");
+			
+		}
+
+		private void greenTambaram() {
+			System.out.println("Greens Tambaram");
+			
+		}
+		private void greensVelacherry() {
+				System.out.println("Greens Velacherry");
+				
+			}
+			private void greensAnnanagar() {
+				System.out.println("Greens Annanagar");
+				
+			}
+			
+			public static void main (String[]args) {
+				GreensTech b=new GreensTech();
+				b.greensAdayar();
+				b.greensAnnanagar();
+				b.greensOmr();
+				b.greensVelacherry();
+				b.greenTambaram();
+				
+			}
+
+}
