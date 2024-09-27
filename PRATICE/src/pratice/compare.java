46e7e9a4562e7ca5f5d84ed8da7cbc28886930fd
sowmiya09-2004 <146425679+sowmiya09-2004@users.noreply.github.com>
2024-09-27 14:52:19 +0530

    Add files via upload

---
+++ PRATICE/src/pratice/compare.java
@@ -0,0 +1,68 @@
+package pratice;
+
+public class compare {
+	public static void main(String[] args) {
+		//reverse
+//		String h="sowmiya";
+//		String u="sowmiya";
+//		String t="";
+//		
+//		for( int i=7-1;i>=0;i--) {
+//			System.out.println(h.charAt(i));
+//			t=t+h.charAt(i);
+//			System.out.println(t);
+//		}
+		
+		
+		
+		///palindrome
+		
+		
+//		String y="malayalam";
+//		String n="";
+//		
+//		for( int i=9-1;i>=0;i--) {
+////			System.out.println(y.charAt(i));
+//			n=n+y.charAt(i);
+//			System.out.println(n);
+//		}
+//		
+//		if(y.equals(n)){
+//			System.out.println("Its is okk");
+//		}
+//		else {
+//			System.out.println("Its not a pali");
+//			
+//		}
+		
+		
+		
+		
+		
+		//adding 
+		int a[]= {12,23,45,56};
+		int sum=0;
+		for(int i=0;i<a.length;i++) {
+		sum=sum+a[i];
+		}
+		System.out.println("the result of:"+sum);
+                                                           		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+	}
+	
